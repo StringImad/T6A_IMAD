b1acc358009e3242ca301acf60d9f0c1e6102c72
imad <imadelfahssi@gmail.com>
2021-02-04 20:45:59 +0100

    Crea al menos dos instancias de cada clase y añádelas a una lista de objetos de la superclase A.

--- src/main/java/biologia/Prueba.java
+++ src/main/java/biologia/Prueba.java
@@ -5,15 +5,30 @@
  */
 package biologia;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author MSI
  */
 public class Prueba {
+
     public static void main(String[] args) {
-        Mamifero perro = new Mamifero("Canino", "mamifero", true, "sexual", 324112, true);
+
+        SeresVivos claseA1 = new SeresVivos("sexual", 12643652, true);
+        SeresVivos claseA2 = new SeresVivos("asexual", 128391, true);
+
+        Animales claseB1 = new Animales("bipedo", "mamifero", true, "sexual", 0, true);
+        Animales claseB2 = new Animales("bipedo", "mamifero", true, "asexual", 0, true);
         
-        System.out.println(perro);
+        Mamifero claseC1 = new Mamifero("Accipitridae", "Ave", true, "sexual", 0, true);
+        Mamifero claseC2 = new Mamifero("Canino", "mamifero", true, "sexual", 324112, true);
         
+        Ave claseD1 = new Ave("Felino", "", true, "sexual", 0, true);
+        Ave claseD3 = new Ave("gallopavo", "ave", true, "sexual", 2375472, true);
+        ArrayList<SeresVivos> array = new ArrayList<>();
+
+        System.out.println(claseC2);
+
     }
 }
