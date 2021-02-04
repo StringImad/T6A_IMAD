920d4b8cf979570868c4cd2c379cab4d4767f868
imad <imadelfahssi@gmail.com>
2021-02-04 21:31:59 +0100

    Correccion metodoA

--- src/main/java/biologia/Prueba.java
+++ src/main/java/biologia/Prueba.java
@@ -24,8 +24,11 @@ public class Prueba {
         Mamifero claseC1 = new Mamifero("Accipitridae", "Ave", true, "sexual", 0, true);
         Mamifero claseC2 = new Mamifero("Canino", "mamifero", true, "sexual", 324112, true);
 
-        Ave claseD1 = new Ave("Felino", "ave", true, "sexual", 0, true);
-        Ave claseD3 = new Ave("gallopavo", "ave", true, "sexual", 2375472, true);
+//        Ave claseD1 = new Ave("Felino", "ave", true, "sexual", 0, true);
+                Ave claseD1 = new Ave("oblicuas", 12, "octagonal", "aguileño", "ave", true, "sexual", 891201, true);
+
+        Ave claseD2 = new Ave("huecas", 4, "tipo iglu", "agaporni", "ave", true, "sexual", 273912, true);
+
         ArrayList<SeresVivos> arraySeres = new ArrayList<>();
 
         arraySeres.add(claseA1);
@@ -35,7 +38,7 @@ public class Prueba {
         arraySeres.add(claseC1);
         arraySeres.add(claseC2);
         arraySeres.add(claseD1);
-        arraySeres.add(claseD3);
+        arraySeres.add(claseD2);
         System.out.println(claseC2);
 
         System.out.println("-----------------------------");
@@ -44,16 +47,36 @@ public class Prueba {
             aux.estaVivo();
             // Conversiones explícitas
             if (aux instanceof Animales) {
-                ((Animales) aux).isEsVertebrado();
+                System.out.println("Es vertebrado?"+ ((Animales) aux).isEsVertebrado());
+              
             }
             if (aux instanceof Mamifero) {
-                Mamifero tmp = (Mamifero) aux;
-                tmp.aguantefrio();
+//                Mamifero aux = (Mamifero) aux;
+//                tmp.aguantefrio();
+//                ((Mamifero) aux).aguantefrio();
+                
+                System.out.println("Aguante al frio: "+((Mamifero) aux).getCantidadPelo());
+
             }
-              if (aux instanceof Ave) {
-                Ave temporal = (Ave) aux;
-                temporal.getNumeroHuevos();
+            if (aux instanceof Ave) {
+//                Ave temporal = (Ave) aux;
+//                temporal.getNumeroHuevos();
+                System.out.println("Cantidad de huevos: "+((Ave) aux).getNumeroHuevos());
+//                ((Ave) aux).getTipoPlumas();
+
             }
         }
+
+//        for (SeresVivos arraySere : arraySeres) {
+//
+//            arraySeres.forEach(System.out::println);
+//        }
+
+//        arraySeres.indexOf(claseD1);
+//
+//        arraySeres.contains(claseB1);
+//
+//        arraySeres.remove(claseA1);
+
     }
 }
